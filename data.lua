f6c8c10b2d9809585db5ad121ec8d15481345dc8
blue42u <blue42u@users.noreply.github.com>
2017-07-12 09:56:17 -0500

    Whoops, act properly with more than one run.

--- data.lua
+++ data.lua
@@ -27,10 +27,15 @@ for l in io.lines() do
 end
 
 local cnt, sum, min, max = 0,0,math.huge,-math.huge
-for t,rs in pairs(d) do for _,r in ipairs(rs) do
-	cnt,sum,min,max = cnt+1, sum+r.r, math.min(min,r.r), math.max(max,r.r)
-	print(('%d %f %f'):format(t, tasks/r.r, (r.u+r.s)/r.r ))
-end end
+for t,rs in pairs(d) do
+	local avgr,avgcpu = 0,0
+	for _,r in ipairs(rs) do
+		cnt,sum,min,max = cnt+1, sum+r.r, math.min(min,r.r), math.max(max,r.r)
+		avgr,avgcpu = avgr + r.r, avgcpu + (r.u+r.s)/r.r
+	end
+	avgr,avgcpu = avgr/#rs, avgcpu/#rs
+	print(('%d %f %f'):format(t, tasks/avgr, avgcpu ))
+end
 
 io.stderr:write((' %d in %.2fs, %.2f/%.2f/%.2f\n'):format(
 	cnt, sum, min, sum/cnt, max))
